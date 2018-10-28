250d89087c82639497c37b14cd6bad96647f08b2
Syed Wajeeh <wajeeh.hasan322@gmail.com>
2018-10-28 09:50:42 -0700

    final version for all pages crawler

--- scraper_scrapy.py
+++ scraper_scrapy.py
@@ -13,3 +13,7 @@ class BookSpider(scrapy.Spider):
 				'title':titles.css("h3>a::attr(title)").extract(),
 				'price':titles.css("div[class='product_price']>p[class='price_color']::text").extract_first()
 			}
+
+		next=response.css("ul[class='pager']>li[class='next']>a::attr(href)").extract_first()
+		if next:
+			yield response.follow(next,self.parse)
